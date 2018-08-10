14792eedea7bb2d14b95b927398ffbf377e7f404
test－mac <team@testmacdeMac-mini.local>
2018-08-10 10:02:15 +0800

    创建工程

---
+++ CTCategories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTCategories
+//
+//  Created by test－mac on 2018/8/10.
+//  Copyright © 2018年 FK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CTCategories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTCategories
+//
+//  Created by test－mac on 2018/8/10.
+//  Copyright © 2018年 FK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
